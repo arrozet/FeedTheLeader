548fd04a2be48ae1c5373bc7f0d9752de4f174d1
edugbau <edugb@uma.es>
2024-04-10 20:11:57 +0200

    Pseudo-Implementación de guardado de preferencias de usuario
    Es necesario revisar la implementación puesto que funciona parcialmente.

--- game/FeedTheLeader/Assets/Scripts/OptionScreen/SettingsMenu.cs
+++ game/FeedTheLeader/Assets/Scripts/OptionScreen/SettingsMenu.cs
@@ -7,6 +7,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 
 
@@ -16,7 +17,9 @@ public class SettingsMenu : MonoBehaviour
     public AudioMixer musicMixer;
     [Header("Resoluciones")]
     public TMP_Dropdown resolutionDropdown;
-    
+    [Header("Slider Música")]
+    public UnityEngine.UI.Slider musicSlider;
+
 
     List<Resolution> resolutions = new List<Resolution>();
     private int width, height;
@@ -29,8 +32,13 @@ public class SettingsMenu : MonoBehaviour
         height = Screen.currentResolution.height;
         refreshRate = Screen.currentResolution.refreshRateRatio;
 
+
+        musicSlider.value = PlayerPrefs.GetFloat("SliderValue", 1);
+        Debug.Log("Valor del slider cargado a" + musicSlider.value);
+
+
         // busco las resoluciones que el pc tiene a nuestra disposición, solo las de mi refreshRate
-        foreach(Resolution res in Screen.resolutions)
+        foreach (Resolution res in Screen.resolutions)
         {
             if (refreshRate.Equals(res.refreshRateRatio))
             {
@@ -42,11 +50,11 @@ public class SettingsMenu : MonoBehaviour
 
         // guardo las resoluciones disponibles en el pc en una lista
         List<string> myResolutionsWithCurrentRefresh = new List<string>();
-        
+
         // guardo el índice para que se muestre correctamente en el display de resoluciones
         int currentResolutionIndex = 0;
-        
-        for (int i=0; i<resolutions.Count; i++)
+
+        for (int i = 0; i < resolutions.Count; i++)
         {
             string newResolution = resolutions[i].width + "x" + resolutions[i].height;
             myResolutionsWithCurrentRefresh.Add(newResolution);
@@ -56,8 +64,8 @@ public class SettingsMenu : MonoBehaviour
             {
                 currentResolutionIndex = i; // va con indices pq los dropdowns van con indices
             }
-                
-            
+
+
 
         }
 
@@ -67,23 +75,23 @@ public class SettingsMenu : MonoBehaviour
         // pongo mi resolución automáticamente (en el dropDown)
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
-        
-}
 
-    public void SetVolume (float sliderVolume)
+    }
+
+    public void SetVolume(float sliderVolume)
     {
         // cambio el volumen del audioMixer al del slider
         // como el sonido es logaritmo, debo aplicar la func logaritmo
         musicMixer.SetFloat("MusicVolume", Mathf.Log10(sliderVolume) * 20);
     }
-    
+
     // Comentado porque ahora mismo no es necesario tener un desplegable para elegir la calidad
     /*public void setQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
     */
-    
+
     public void SetFullscreen(bool fullScreen)
     {
         // cambio el modo pantalla completa en función de si está el checkmark guardado o no
@@ -97,5 +105,13 @@ public class SettingsMenu : MonoBehaviour
         // actualizo de verdad mi resolución
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
-    
+
+    //Parte de guardado: Edu
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.SetFloat("SliderValue", musicSlider.value);
+        Debug.Log("Valor del slider guardado a " + musicSlider.value);
+    }
+
 }
