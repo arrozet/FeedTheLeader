fc3c8a15e50e8bd458f0cea9f00014478b03f991
elyez54 <eloygonzalez@uma.es>
2024-04-13 20:03:18 +0200

    fix bug build
    unity editor malo hay que ponerlo con un if using

--- game/FeedTheLeader/Assets/Scripts/imageMenuAnimationScript.cs
+++ game/FeedTheLeader/Assets/Scripts/imageMenuAnimationScript.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.Animations;
 
 public class imageMenuAnimationScript : MonoBehaviour
 {
