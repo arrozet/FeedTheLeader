8f55395ff4380f62e2fe7ce533b1a3261e803f11
edugbau <edugb@uma.es>
2024-05-25 15:50:00 +0200

    Implementado cambio de fase del líder

---
+++ game/FeedTheLeader/Assets/LeaderButton.cs
@@ -0,0 +1,57 @@
+//Autor: Edu
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderButton : MonoBehaviour
+{
+
+    public Sprite[] leaderSprites;
+    public PointsManager pointsManager;
+    private Image botonImage;  // Referencia al componente Image del botón
+    private float salto = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        botonImage = GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pointsManager.currentScore < Mathf.Pow(10, 1 * salto))
+        {
+            botonImage.sprite = leaderSprites[0];
+        } else if (pointsManager.currentScore < Mathf.Pow(10, 2 * salto))
+        {
+            botonImage.sprite = leaderSprites[1];
+        } else if (pointsManager.currentScore < Mathf.Pow(10, 3 * salto))
+        {
+            botonImage.sprite= leaderSprites[2];
+        }
+        else if (pointsManager.currentScore < Mathf.Pow(10, 4 * salto))
+        {
+            botonImage.sprite = leaderSprites[3];
+        }
+        else if (pointsManager.currentScore < Mathf.Pow(10, 5 * salto))
+        {
+            botonImage.sprite = leaderSprites[4];
+        }
+        else if (pointsManager.currentScore < Mathf.Pow(10, 6 * salto))
+        {
+            botonImage.sprite = leaderSprites[5];
+        }
+        else if (pointsManager.currentScore < Mathf.Pow(10, 7 * salto))
+        {
+            botonImage.sprite = leaderSprites[6];
+        }
+        else if (pointsManager.currentScore < Mathf.Pow(10, 8 * salto))
+        {
+            botonImage.sprite = leaderSprites[7];
+        }
+    }
+}
